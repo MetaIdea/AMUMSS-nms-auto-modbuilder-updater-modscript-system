e2129be80226076ee507193c18178698e50a5e7d
azilvl <73azim@gmail.com>
2020-01-26 18:00:32 +0330

    Add comment support for the options file also changes its extension to .yaml

--- BuildMod.bat
+++ BuildMod.bat
@@ -1,17 +1,34 @@
 @echo off
 setlocal EnableDelayedExpansion
 set "args="
-if not exist options.txt (    
-    echo includeExtraFiles=ask> OPTIONS.txt
-    echo rebuildMod=ask>> options.txt
-    echo combine=ask>> options.txt
-    echo copy=ask>> options.txt
-    echo updateMBINCompiler=ask>> options.txt
-    echo checkForConflicts=ask>> options.txt
-    echo recreatePakList=ask>> options.txt
-    echo|set /p="recreateMapFileTrees=ask">> options.txt
+set "rootPath=%~dp0"
+set "optionsFile=!rootPath!\OPTIONS.yaml"
+
+if not exist %optionsFile% (
+    echo # These are the default options used by BuildMod.bat to build your mods.>%optionsFile%
+    echo # In each one of them you can either specify a default value or put 'ask'.>> %optionsFile%
+    echo # Any option with a value of ask will result in the builder to ask you the question during the build process.>> %optionsFile%
+    echo.>>%optionsFile%
+    echo # Possible Values:>> %optionsFile%
+    echo # combine = ask, none, simple, composite, numericSuffix>> %optionsFile%
+    echo # copy =  ask, none, all, some>> %optionsFile%
+    echo.>>%optionsFile%
+    echo # All the other options are flags with possible values of: ask, true, false>> %optionsFile%
+    echo.>>%optionsFile%
+    echo combine: ask>> %optionsFile%
+    echo copy: ask>> %optionsFile%
+    echo includeExtraFiles: ask>> %optionsFile%
+    echo rebuildMod: ask>> %optionsFile%
+    echo updateMBINCompiler: ask>> %optionsFile%
+    echo checkForConflicts: ask>> %optionsFile%
+    echo recreatePakList: ask>> %optionsFile%
+    echo|set /p="recreateMapFileTrees: ask">> %optionsFile%
 )
-for /f "tokens=1,2 delims==" %%G in (options.txt) do (
+
+rem The added space character in `delims=: ` is to support same line comments in the yaml file. 
+rem As a side effect typing `key value` instead of `key: value` would also work.
+rem If we decide to just not support same line comments we should change it to `delims=:`
+for /f "tokens=1,2 eol=# delims=: " %%G in (%optionsFile%) do (    
     set "args=!args!-%%G %%~H "
 )
-Builder.bat %args%
+!rootPath!\Builder.bat %args%
