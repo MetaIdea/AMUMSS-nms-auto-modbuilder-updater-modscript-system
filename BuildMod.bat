504c1fce76f3204b19ccdcb1b3b6831c33401431
azilvl <73azim@gmail.com>
2020-01-26 04:03:27 +0330

    Auto generate the options file when its missing

--- BuildMod.bat
+++ BuildMod.bat
@@ -1,6 +1,16 @@
 @echo off
 setlocal EnableDelayedExpansion
 set "args="
+if not exist options.txt (    
+    echo includeExtraFiles=ask> OPTIONS.txt
+    echo rebuildMod=ask>> options.txt
+    echo combine=ask>> options.txt
+    echo copy=ask>> options.txt
+    echo updateMBINCompiler=ask>> options.txt
+    echo checkForConflicts=ask>> options.txt
+    echo recreatePakList=ask>> options.txt
+    echo|set /p="recreateMapFileTrees=ask">> options.txt
+)
 for /f "tokens=1,2 delims==" %%G in (options.txt) do (
     set "args=!args!-%%G %%~H "
 )
